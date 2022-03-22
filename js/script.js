e9b51225afe0ff4b2187fe7ac0daa5ba22e206d9
Alena Nahornaya <nahornaya.alena@gmail.com>
2022-03-22 16:52:16 +0100

    onChangeBackgroundClick and init function created

--- js/script.js
+++ js/script.js
@@ -1,31 +1,39 @@
-let photo = document.querySelector(".js-header__photo");
-let hidePhotoButton = document.querySelector(".js-header__hidePhotoButton");
-let backgroundChangeButton = document.querySelector(".js-header__backgroundChangeButton");
-let content = document.querySelector(".js-content");
+{
+    let photo = document.querySelector(".js-header__photo");
+    let hidePhotoButton = document.querySelector(".js-header__hidePhotoButton");
 
-let aboutMe = document.querySelector(".js-navigation__linkAboutMe");
-let education = document.querySelector(".js-navigation__linkEducation");
-let foreignLanguages = document.querySelector(".js-navigation__linkForeignLanguages")
-let hobby = document.querySelector(".js-navigation__linkHobby");
-let contact = document.querySelector(".js-navigation__linkContact");
+    hidePhotoButton.addEventListener("click", () => {
+        photo.classList.toggle("header__hiddenPhoto");
 
-hidePhotoButton.addEventListener("click", () => {
-    photo.classList.toggle("header__hiddenPhoto");
+        hidePhotoButton.innerText = (photo.classList.contains("header__hiddenPhoto"))
+            ? "Pokaż zdjęcie"
+            : "Ukryj zdjęcie";
+    });
 
-    hidePhotoButton.innerText = (photo.classList.contains("header__hiddenPhoto"))
-        ? "Pokaż zdjęcie"
-        : "Ukryj zdjęcie";
-});
+    const onChangeBackgroundClick = () => {
+        const content = document.querySelector(".js-content");
+        const aboutMe = document.querySelector(".js-navigation__linkAboutMe");
+        const education = document.querySelector(".js-navigation__linkEducation");
+        const foreignLanguages = document.querySelector(".js-navigation__linkForeignLanguages")
+        const hobby = document.querySelector(".js-navigation__linkHobby");
+        const contact = document.querySelector(".js-navigation__linkContact");
 
-backgroundChangeButton.addEventListener("click", () => {
-    content.classList.toggle("darkContent");
-    aboutMe.classList.toggle("js-navigation__link--dark");
-    education.classList.toggle("js-navigation__link--dark");
-    foreignLanguages.classList.toggle("js-navigation__link--dark");
-    hobby.classList.toggle("js-navigation__link--dark");
-    contact.classList.toggle("js-navigation__link--dark");
+        content.classList.toggle("darkContent");
+        aboutMe.classList.toggle("js-navigation__link--dark");
+        education.classList.toggle("js-navigation__link--dark");
+        foreignLanguages.classList.toggle("js-navigation__link--dark");
+        hobby.classList.toggle("js-navigation__link--dark");
+        contact.classList.toggle("js-navigation__link--dark");
 
-    backgroundChangeButton.innerText = (content.classList.contains("darkContent"))
-        ? "Włącz jasny motyw"
-        : "Włącz ciemny motyw";
-});
+        backgroundChangeButton.innerText = (content.classList.contains("darkContent"))
+            ? "Włącz jasny motyw"
+            : "Włącz ciemny motyw";
+    };
+
+    const init = () => {
+        const backgroundChangeButton = document.querySelector(".js-header__backgroundChangeButton");
+        backgroundChangeButton.addEventListener("click", onChangeBackgroundClick);
+    };
+    
+    init();
+}
